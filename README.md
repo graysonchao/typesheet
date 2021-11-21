29c0f180898829690c67b87b3eebec98d1e8ffb0
Grayson Chao <grayson.chao@gmail.com>
2021-11-20 23:22:36 -0800

    make repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# typesheet
+
+typesheet is a tiny Python script for creating transparent PNG spritesheets from TrueType (.ttf) fonts.
+
+I made it because I couldn't find an easy option online while developing a game.
+
+I welcome contributions, but I don't plan to spend more than a few minutes a month maintaining this repo.
+
+## How to use
+
+`pip install pillow tcod numpy PIL`
+
+There might be more dependencies missing. If so, you should install them the same way.  I'll accept PRs to update the list of deps, but I rarely write Python and can't be bothered to set up a clean-room environment to figure them out myself.
+
+### Also
+
+This code is released under whatever licenses are appropriate given the dependencies.
+I claim no ownership etc. and you can use it for whatever you want. I don't care.
---
+++ make_tileset.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import argparse
+import io
+import numpy as np
+from numpy.typing import ArrayLike, NDArray
+import tcod
+import PIL.Image as Image
+import PIL.ImageFont as ImageFont
+import PIL.ImageDraw as ImageDraw
+
+from tcod.loader import lib
+from tcod.tileset import CHARMAP_CP437
+
+CP437_LEN=256
+IMAGE_W_CELLS=16
+IMAGE_H_CELLS=16
+
+def write_cp437(font_path, width_px, height_px): # -> ndarray[uint8]
+    tileset = tcod.tileset.load_truetype_font(
+        font_path,
+        tile_width=width_px,
+        tile_height=height_px,
+    )
+    for i in range(256):
+        tileset.remap(tcod.tileset.CHARMAP_CP437[i], i)
+
+    buf = np.zeros(
+            shape=(IMAGE_W_CELLS, IMAGE_H_CELLS),
+            dtype=tcod.console.Console.DTYPE,
+            order="F"
+    )
+    for x in range(IMAGE_W_CELLS):
+        for y in range(IMAGE_H_CELLS):
+            buf[x, y] = tcod.tileset.CHARMAP_CP437[(IMAGE_W_CELLS * x) + y]
+
+    return buf
+
+def render(buf: NDArray[np.uint8], font: ImageFont, width_px: int, height_px: int):
+    canvas_w = IMAGE_W_CELLS * width_px
+    canvas_h = IMAGE_H_CELLS * height_px
+    canvas = Image.new('RGBA', (canvas_w, canvas_h))
+    draw = ImageDraw.Draw(canvas)
+
+    for cellX in range(IMAGE_W_CELLS):
+        for cellY in range(IMAGE_H_CELLS):
+            draw.text((width_px * cellX, height_px * cellY), chr(buf[cellY, cellX][0]), fill='white', font=font)
+    return canvas
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input_file")
+parser.add_argument("width_px", type=int, help="width of a glpyh('s bounding box) in px")
+parser.add_argument("height_px", type=int, help="height of a glyph('s bounding box) in px")
+parser.add_argument("output_file")
+args = parser.parse_args()
+
+buf = write_cp437(args.input_file, args.width_px, args.height_px)
+font = ImageFont.truetype(args.input_file, size=args.height_px)
+render(buf, font, args.width_px, args.height_px).save(args.output_file)
